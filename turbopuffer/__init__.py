1eb4338c36e67f2a4105f0dc746d55110d372e72
Jacob Wirth <xthexder@users.noreply.github.com>
2024-01-25 15:55:13 +0000

    Fix numpy compatibility with non-fast JSON serializer

--- turbopuffer/__init__.py
+++ turbopuffer/__init__.py
@@ -1,4 +1,5 @@
 import os
+import sys
 api_key = os.environ.get('TURBOPUFFER_API_KEY')
 api_base_url = os.environ.get('TURBOPUFFER_API_BASE_URL', 'https://api.turbopuffer.com/v1')
 upsert_batch_size = 5_000
@@ -8,7 +9,19 @@ try:
     def dump_json_bytes(obj): return orjson.dumps(obj, option=orjson.OPT_SERIALIZE_NUMPY)
 except ImportError:
     import json
-    def dump_json_bytes(obj): return json.dumps(obj).encode()
+
+    class NumpyEncoder(json.JSONEncoder):
+        def default(self, obj):
+            if 'numpy' in sys.modules:
+                if isinstance(obj, sys.modules['numpy'].integer):
+                    return int(obj)
+                elif isinstance(obj, sys.modules['numpy'].floating):
+                    return float(obj)
+                elif isinstance(obj, sys.modules['numpy'].ndarray):
+                    return obj.tolist()
+            return json.JSONEncoder.default(self, obj)
+
+    def dump_json_bytes(obj): return json.dumps(obj, cls=NumpyEncoder).encode()
 
 from turbopuffer.version import VERSION
 from turbopuffer.namespace import Namespace
