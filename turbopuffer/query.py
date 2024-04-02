66ddccefe29d73f2f1ea38628ecce9cb9487c176
Justin Li <git@justinli.net>
2024-04-02 15:27:50 -0400

    support new filter syntax

--- turbopuffer/query.py
+++ turbopuffer/query.py
@@ -3,17 +3,16 @@ import sys
 from typing import Optional, List, Tuple, Union, Dict
 from enum import Enum
 
+# Refer to turbopuffer docs for valid operator names
+FilterOperator = str
 
-class FilterMatch(Enum):
-    EQ = 'Eq'
-    NOT_EQ = 'NotEq'
-    IN = 'In'
-    GLOB = 'Glob'
-    NOT_GLOB = 'NotGlob'
+FilterValue = str | int | List[str] | List[int]
+FilterCondition = Tuple[str, FilterOperator, FilterValue]
 
+LegacyFilterCondition = Tuple[FilterOperator, FilterValue]
+LegacyFilterDict = Dict[str, List[LegacyFilterCondition]]
 
-FilterTuple = Tuple[FilterMatch, Union[List[str], str, Union[List[int], int]]]
-
+Filters = Tuple[str, List[Filters]] | FilterCondition | LegacyFilterDict
 
 @dataclass
 class VectorQuery:
@@ -22,7 +21,7 @@ class VectorQuery:
     top_k: int = 10
     include_vectors: bool = False
     include_attributes: Optional[Union[List[str], bool]] = None
-    filters: Optional[Dict[str, List[FilterTuple]]] = None
+    filters: Optional[Filters] = None
 
     def from_dict(source: dict) -> 'VectorQuery':
         return VectorQuery(
@@ -45,16 +44,5 @@ class VectorQuery:
             if not isinstance(self.include_attributes, list) and not isinstance(self.include_attributes, bool):
                 raise ValueError('VectorQuery.include_attributes must be a list or bool, got:', type(self.include_attributes))
         if self.filters is not None:
-            if not isinstance(self.filters, dict):
-                raise ValueError('VectorQuery.filters must be a dict, got:', type(self.filters))
-            else:
-                for name, filter in self.filters.items():
-                    if isinstance(filter, list):
-                        if len(filter) == 2 and isinstance(filter[0], str):
-                            # Support passing a single filter instead of a list
-                            self.filters[name] = [filter]
-                        elif len(filter) > 0 and not isinstance(filter[0], list):
-                            raise ValueError(f'VectorQuery.filters expected a list of filters for key {name}, got list of:',
-                                             type(filter[0]))
-                    else:
-                        raise ValueError(f'VectorQuery.filters expected a list for key {name}, got:', type(filter))
+            if not isinstance(self.filters, dict) and not isinstance(self.filters, list):
+                raise ValueError('VectorQuery.filters must be a dict or list, got:', type(self.filters))
