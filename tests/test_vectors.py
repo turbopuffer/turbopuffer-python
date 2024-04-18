eaf2d7c404e3b87aaecffc7d99f4c811ed03939f
Simon Eskildsen <sirup@sirupsen.com>
2024-04-18 14:42:20 -0400

    fix test for new approx count

--- tests/test_vectors.py
+++ tests/test_vectors.py
@@ -296,7 +296,7 @@ def test_read_metadata():
 
     assert ns.exists()
     assert ns.dimensions() == 2
-    assert ns.approx_count() == 92
+    assert ns.approx_count() == 98
 
     all_ns = tpuf.namespaces()
     assert ns in list(all_ns)
