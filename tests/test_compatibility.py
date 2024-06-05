1c4d81609db20ba8e8b746c4131b70f5a93108cd
Morgan Gallant <morgan@morgangallant.com>
2024-06-05 09:18:10 -0700

    Merge PR for #15 (#22)
    * lint: format using Black
    
    * feat: add AsyncBackend
    
    - use httpx instead of requests
    - allow passing httpx clients (sync and/or async)
    
    * fix: user-agent header typo
    
    * upsert: add distance_metric on upsert
    
    * Bump version 0.1.8
    
    * remove usage of implicit And (#19)
    
    * support new filter syntax (#20)
    
    * update version
    
    * fix test for new approx count (#21)
    
    * wip merge
    
    Signed-off-by: Morgan Gallant <morgan@morgangallant.com>
    
    * fix warnings
    
    Signed-off-by: Morgan Gallant <morgan@morgangallant.com>
    
    * remove "distances" from upsert json
    
    Signed-off-by: Morgan Gallant <morgan@morgangallant.com>
    
    * fix merge issues
    
    Signed-off-by: Morgan Gallant <morgan@morgangallant.com>
    
    ---------
    
    Signed-off-by: Morgan Gallant <morgan@morgangallant.com>
    Co-authored-by: Sourabh Mundhra <sourabh@foyer.work>
    Co-authored-by: Simon Eskildsen <sirup@sirupsen.com>
    Co-authored-by: Justin Li <git@justinli.net>

--- tests/test_compatibility.py
+++ tests/test_compatibility.py
@@ -1,18 +1,15 @@
-import turbopuffer as tpuf
 import tests
+import turbopuffer as tpuf
 
 try:
     import numpy as np
 
     def test_numpy_support():
-        ns = tpuf.Namespace(tests.test_prefix + 'numpy')
+        ns = tpuf.Namespace(tests.test_prefix + "numpy")
         data = np.random.uniform(low=-1.5, high=1.5, size=(100, 64))
 
         # Test column upsert
-        ns.upsert(
-            ids=np.arange(0, data.shape[0]),
-            vectors=data
-        )
+        ns.upsert(ids=np.arange(0, data.shape[0]), vectors=data)
         vecs = ns.vectors()
         for i, vec in enumerate(vecs):
             assert vec.id == i
@@ -28,7 +25,7 @@ try:
         # Test list of numpy data
         ns.upsert(
             ids=[np.int64(i) for i in range(0, data.shape[0])],
-            vectors=[[np.float64(v) for v in row] for row in data]
+            vectors=[[np.float64(v) for v in row] for row in data],
         )
         vecs = ns.vectors()
         for i, vec in enumerate(vecs):
@@ -36,13 +33,18 @@ try:
             assert np.allclose(vec.vector, data[i])
 
         # Test query with numpy vector
-        result = ns.query(vector=data[5], distance_metric="cosine_distance", top_k=1, include_vectors=True)
+        result = ns.query(
+            vector=data[5],
+            distance_metric="cosine_distance",
+            top_k=1,
+            include_vectors=True,
+        )
         assert len(result) == 1
         assert result[0].id == 5
         assert np.allclose(result[0].vector, data[5])
 
         ns.delete_all()
 
-    print('Loaded numpy tests')
+    print("Loaded numpy tests")
 except ImportError:
-    print('Skipped numpy tests')
+    print("Skipped numpy tests")
