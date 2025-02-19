196860c22ee80eb19624c75c007930dc650c46f7
Arash-11 <arashnawy@gmail.com>
2025-02-18 19:15:45 -0500

    return rows_affected in delete_in_filter

--- tests/test_vectors.py
+++ tests/test_vectors.py
@@ -512,12 +512,14 @@ def test_delete_by_filter():
         {'id': 9, 'vector': [0.7, 0.7], 'attributes': {'a': 3}},
     ], distance_metric='euclidean_squared')
 
-    ns.delete_by_filter(['a', 'Eq', 42]) # no-op, nothing matches
+    res = ns.delete_by_filter(['a', 'Eq', 42]) # no-op, nothing matches
+    assert res == 0
 
     results = ns.vectors()
     assert len(results) == 5, "Got wrong number of vectors back"
 
-    ns.delete_by_filter(['a', 'Eq', 1])
+    res = ns.delete_by_filter(['a', 'Eq', 1])
+    assert res == 2
 
     results = ns.vectors()
     assert len(results) == 3, "Got wrong number of vectors back"
@@ -525,7 +527,8 @@ def test_delete_by_filter():
     assert results[1].id == 7
     assert results[2].id == 9
 
-    ns.delete_by_filter(['a', 'Gt', 0])
+    res = ns.delete_by_filter(['a', 'Gt', 0])
+    assert res == 2
 
     results = ns.vectors()
     assert len(results) == 1, "Got wrong number of vectors back"
--- turbopuffer/namespace.py
+++ turbopuffer/namespace.py
@@ -341,12 +341,14 @@ class Namespace:
         assert response.get('content', dict()).get('status', '') == 'OK', f'Invalid delete() response: {response}'
         self.metadata = None  # Invalidate cached metadata
 
-    def delete_by_filter(self, filters: Filters) -> None:
+    def delete_by_filter(self, filters: Filters) -> int:
         response = self.backend.make_api_request('namespaces', self.name, payload={
             'delete_by_filter': filters
         })
-        assert response.get('content', dict()).get('status', '') == 'OK', f'Invalid delete_by_filter() response: {response}'
+        response_content = response.get('content', dict())
+        assert response_content.get('status', '') == 'OK', f'Invalid delete_by_filter() response: {response}'
         self.metadata = None  # Invalidate cached metadata
+        return response_content.get('rows_affected')
 
     @overload
     def query(self,
