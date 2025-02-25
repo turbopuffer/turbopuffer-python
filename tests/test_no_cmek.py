bbd54b63d32df50083741b56117278140aa6e072
Arash <62180768+Arash-11@users.noreply.github.com>
2025-02-25 11:17:35 -0500

    Add encryption param (#58)
    * add encryption param
    
    * bump to v0.1.31
    
    * rename test_no_encryption -> test_no_cmek

---
+++ tests/test_no_cmek.py
@@ -0,0 +1,24 @@
+import turbopuffer as tpuf
+from turbopuffer.error import APIError
+import tests
+
+
+def test_no_cmek():
+    ns = tpuf.Namespace(tests.test_prefix + 'no_cmek')
+
+    try:
+        ns.upsert(
+            {
+                'ids': [1, 2],
+                'vectors': [[0.1, 0.1], [0.2, 0.2]],
+            },
+            distance_metric='cosine_distance',
+            encryption={
+                'cmek': {
+                    'key_name': 'mykey'
+                }
+            }
+        )
+        assert False, 'Using CMEK is only available as part of enterprise offerings.'
+    except APIError as err:
+        assert err.args == ('error (HTTP 400): 💔 CMEK is not currently enabled in this cluster',)
