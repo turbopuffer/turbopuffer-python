651ab0f8ee76439be99609329839de0c145b3b5a
Arash <62180768+Arash-11@users.noreply.github.com>
2025-04-06 01:03:15 -0400

    update performance metrics (#70)
    * add query_execution_time to performance metrics
    
    * fix cache_hit_ratio + add cache_temperature
    
    * add exhaustive_search_count to performance metrics
    
    * bump to v0.1.36
    
    * fix exhaustive_search_count
    
    * int-ify exhaustive_search_count
    
    * skill issues
    
    * -_-

---
+++ tests/test_perf_metrics.py
@@ -0,0 +1,26 @@
+import turbopuffer as tpuf
+import tests
+
+
+def test_perf_metrics():
+    ns = tpuf.Namespace(tests.test_prefix + 'perf_metrics')
+
+    ns.upsert([
+        {'id': 1, 'vector': [0.1, 0.1]},
+        {'id': 2, 'vector': [0.2, 0.2], 'attributes': {'hello': 'world', 'test': 'rows'}},
+    ], distance_metric='cosine_distance')
+
+    results = ns.query(
+        vector=[0.0, 0.3],
+        distance_metric='cosine_distance',
+        include_attributes=['hello'],
+        filters=['hello', 'Eq', 'world'],
+    )
+
+    metrics = results.performance
+
+    assert type(metrics['cache_hit_ratio']) is float
+    assert metrics['cache_temperature'] in ['hot', 'warm', 'cold']
+    assert metrics['exhaustive_search_count'] == 2
+    assert metrics['server_time'] > 0
+    assert metrics['query_execution_time'] > 0
