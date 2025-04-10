22bbc0d06784c4ed1bc1b2a45ac7347995270f7c
Justin Li <git@justinli.net>
2025-04-09 20:39:50 -0400

    Revert "wip"
    This reverts commit 5fbf42af8fc8cd334402ec6700bcc0b66ad9797d.

--- turbopuffer/__init__.py
+++ turbopuffer/__init__.py
@@ -9,13 +9,6 @@ connect_timeout = 10 # seconds
 read_timeout = 180 # seconds
 
 def encode_internal_types(obj):
-    if 'numpy' in sys.modules:
-        if isinstance(obj, sys.modules['numpy'].integer):
-            return int(obj)
-        elif isinstance(obj, sys.modules['numpy'].floating):
-            return float(obj)
-        elif isinstance(obj, sys.modules['numpy'].ndarray):
-            return obj.tolist()
     if isinstance(obj, VectorRow):
         o = {
             'id': obj.id,
@@ -54,7 +47,7 @@ try:
         return obj
 
     def dump_json_bytes(obj):
-        return orjson.dumps(obj, default=orjson_default)
+        return orjson.dumps(obj, default=orjson_default, option=orjson.OPT_SERIALIZE_NUMPY)
 except ImportError:
     import json
 
