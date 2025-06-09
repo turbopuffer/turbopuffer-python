b38de2eb4e063b0d9534862cfc517764ca1f56b1
Nikhil Benesch <nikhil.benesch@gmail.com>
2025-06-09 03:55:13 -0400

    feat: improve deprecation warnings

--- src/turbopuffer/lib/namespace.py
+++ src/turbopuffer/lib/namespace.py
@@ -29,11 +29,26 @@ class Namespace(namespaces.NamespacesResource, NamespaceMixin):
     def __init__(self, client: Turbopuffer) -> None:
         super().__init__(client)
 
+    # TODO(benesch): remove these shims when sufficient time has passed (say, Sep 2025).
+
     @deprecated("method removed; see https://github.com/turbopuffer/turbopuffer-python/blob/main/UPGRADING.md for help")
     def metadata(self) -> None:
         raise NotImplementedError(
             "the Namespace.metadata() method has been removed; see https://github.com/turbopuffer/turbopuffer-python/blob/main/UPGRADING.md for help"
         )
+
+    @deprecated("method removed; see https://github.com/turbopuffer/turbopuffer-python/blob/main/UPGRADING.md for help")
+    def dimensions(self) -> None:
+        raise NotImplementedError(
+            "the Namespace.dimensions() method has been removed; see https://github.com/turbopuffer/turbopuffer-python/blob/main/UPGRADING.md for help"
+        )
+
+    @deprecated("method removed; see https://github.com/turbopuffer/turbopuffer-python/blob/main/UPGRADING.md for help")
+    def approx_count(self) -> None:
+        raise NotImplementedError(
+            "the Namespace.approx_count() method has been removed; see https://github.com/turbopuffer/turbopuffer-python/blob/main/UPGRADING.md for help"
+        )
+
 
 
 class NamespaceWithRawResponse(namespaces.NamespacesResourceWithRawResponse, NamespaceMixin):
