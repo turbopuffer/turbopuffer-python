fae8c4d7f79d52f7996c1638492b28b9b72874aa
Nikhil Benesch <nikhil.benesch@gmail.com>
2025-06-20 00:35:51 -0400

    fix: strict types for new QueryParam type
    With a fix for a typing crash in Python 3.9+.

--- src/turbopuffer/types/query_param.py
+++ src/turbopuffer/types/query_param.py
@@ -5,6 +5,11 @@ from __future__ import annotations
 from typing import Dict
 from typing_extensions import TypedDict
 
+# Without this seemingly unnecessary import of `RankByText`, calling
+# `multi_query` fails at runtime claiming `RankByText` is unknown. It seems to
+# be some sort of bug with scoping and recursive types (`RankByText` is
+# recursive).
+from .custom import Filter, RankBy, RankByText as RankByText, AggregateBy
 from .distance_metric import DistanceMetric
 from .include_attributes_param import IncludeAttributesParam
 
@@ -12,7 +17,7 @@ __all__ = ["QueryParam"]
 
 
 class QueryParam(TypedDict, total=False):
-    aggregate_by: Dict[str, object]
+    aggregate_by: Dict[str, AggregateBy]
     """
     Aggregations to compute over all documents in the namespace that match the
     filters.
@@ -21,7 +26,7 @@ class QueryParam(TypedDict, total=False):
     distance_metric: DistanceMetric
     """A function used to calculate vector similarity."""
 
-    filters: object
+    filters: Filter
     """Exact filters for attributes to refine search results for.
 
     Think of it as a SQL WHERE clause.
@@ -30,7 +35,7 @@ class QueryParam(TypedDict, total=False):
     include_attributes: IncludeAttributesParam
     """Whether to include attributes in the response."""
 
-    rank_by: object
+    rank_by: RankBy
     """How to rank the documents in the namespace."""
 
     top_k: int
