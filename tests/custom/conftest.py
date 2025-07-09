b7e8297f705be97edee77f41487c51ae7bc975cb
Nikhil Benesch <nikhil.benesch@gmail.com>
2025-07-09 15:45:23 -0400

    fix: don't set region in tests if base URL doesn't support it

--- tests/custom/conftest.py
+++ tests/custom/conftest.py
@@ -1,16 +1,22 @@
+import os
 from typing import Iterator, AsyncIterator
 
 import pytest
 
 from turbopuffer import Turbopuffer, AsyncTurbopuffer
 
+# If the base URL contains a {region} placeholder, use gcp-us-central1.
+region = None
+if "{region}" in os.getenv("TURBOPUFFER_BASE_URL", "{region}"):
+    region = "gcp-us-central1"
+
 
 @pytest.fixture(scope="session")
 def tpuf() -> Iterator[Turbopuffer]:
-    with Turbopuffer(region="gcp-us-central1") as tpuf:
+    with Turbopuffer(region=region) as tpuf:
         yield tpuf
 
 
 @pytest.fixture(scope="session")
 async def async_tpuf() -> AsyncIterator[AsyncTurbopuffer]:
-    yield AsyncTurbopuffer(region="gcp-us-central1")
+    yield AsyncTurbopuffer(region=region)
